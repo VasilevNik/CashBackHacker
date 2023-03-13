1e55d20a73f4f55c6da3e5b37db319e4e0468a6a
Vasilev Nik <heshelbe@inbox.ru>
2023-03-13 17:10:39 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void calculateRequiredAmountIfFiveHundredSpent() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateRequiredAmountIfNineHundredAndNinetyNineSpent() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateRequiredAmountIfThousandSpent() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+}
